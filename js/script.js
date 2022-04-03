02b1035d32e982be7136c711ac2978acffea582f
Mohd Rejoan <wearecrx@gmail.com>
2022-04-03 17:38:07 +0600

    loading screen complete

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// loading animation
+
+const loadingTl = gsap.timeline();
+
+const loadingPathSelect =
+    "#loading #path__1,#loading #path__2,#loading #path__3,#loading #path__4 path";
+
+loadingTl
+    .from(loadingPathSelect, {
+        opacity: 0,
+        duration: 1,
+      stagger: 0.25,
+      delay: 0.8,
+        y: "20px"
+    })
+    .fromTo(
+        loadingPathSelect,
+        {
+            y: "0px",
+        },
+        {
+            y: "25",
+            stagger: 0.25,
+            yoyo: true,
+            repeat: -1,
+        }
+)
+  // .to(loadingPathSelect, {
+  // scale: 0,
+  // opacity: 0,
+  // duration: 1,
+  // delay: 3
+  // })
+  .to("#loading svg", {
+    y: '-100%',
+    opacity: 0,
+    onComplete: function () {
+      gsap.to("#loading svg", {
+        display: 'none'
+      })
+    }
+  })
+  .to('#loading', {
+  height: "0",
+  duration: 1.2,
+    background: 'white',
+})
